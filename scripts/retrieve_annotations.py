8ba41780f739785002a00a5266671f5d70276315
Marylette B. Roa <maryletteroa@gmail.com>
2019-04-20 16:41:53 +0800

    add script to retrieve annotations

--- scripts/retrieve_annotations.py
+++ scripts/retrieve_annotations.py
@@ -2,7 +2,19 @@
 # @Author: Marylette B. Roa
 # @Date:   2019-04-19 10:36:19
 # @Last Modified by:   Marylette B. Roa
-# @Last Modified time: 2019-04-19 16:20:25
+# @Last Modified time: 2019-04-20 16:38:22
+
+"""
+This script accesses the European PMC API using a user-supplied list of publication ids
+    and returns a tab-delimied file containing annotated Organism terms present in each of these publications
+
+comment tagged [MODIFY THIS]
+    Only the "Organisms" annotations are retrieved for now 
+    Also query data parameters can be further customized
+
+API documentation at: https://europepmc.org/AnnotationsApi#!/annotations45api45controller/getAnnotationsArticlesByIdsUsingGET
+"""
+
 
 import requests
 import json
@@ -11,103 +23,184 @@ import datetime
 from sys import  argv, exit
 import  re
 
-def getOrganismsInfo(results):
-    annotations = results["annotations"]
-    terms = {}
-    for annotation in annotations:
-        exact = annotation["exact"]
-        section = annotation["section"].split(' ')[0]
-        if section not in terms:
-            terms[section] = []
-            terms[section].append(exact)
-        else:
-            if exact.lower() not in map(str.lower,terms[section]):
-                terms[section].append(exact)
-    source = {}
-    for h in ["extId", "pmcid", "source"]:
-        try:
-            source[h] = results[h]
-        except KeyError:
-            source[h] = ""
-    organisms_info = {**source, **terms}
-    return organisms_info
+def retrieveAnnotations(ids,type_):
 
-def runRetrieveAnnotation(ids, type_):
-# check if there is an outbound network first
+    # didn't break this up into smaller functions (e.g. retrieve, parse, collate) but could be
+
+    infos = []
+    counter_ids = 0
+
+    # connect to API
     URL = "https://www.ebi.ac.uk/europepmc/annotations_api/annotationsByArticleIds?"
     for id_ in ids:
+        counter_ids += 1
+        print(f"Retrieving annotations for {id_} ... {counter_ids}", end="\r") #\r for carriage return
         data = {
             "articleIds" : f"{id_}",
-            "type" : f"{type_}", #only one type to retreive per run
+            "type" : f"{type_}",
             "section" : "",
             "provider" : "",
             "format" : "",
-        }
+        } ## [MODIFY THIS]
+
         try: 
             response = requests.get(URL, params=data)
+            # check if response == 200 
             if response:
                 try:
                     results = json.loads(response.text[1:-1]) # remove start and end parenthesis to convert into json properly
-                    if type_ == "Organisms":
-                        annotations = getOrganismsInfo(results)
-                        yield annotations
-                    # if type_ == "Accession Numbers":
-                    #     accession_numbers_info = getAccessionNumbersInfo(results)
                 except json.decoder.JSONDecodeError:
                     # because API does not otherwise return a non-200
-                    pprint(f"The query did not parse correctly. Check the input.")# but returned with the following message: \n\t{response.text}")
+                    pprint(f"The query did not parse correctly")# but returned with the following message: \n\t{response.text}")
+                    exit()
             else:
                 print(f"An error occurred. {response.status_code}")
+                exit()
         except requests.exceptions.ConnectionError:
             print("\nA connection error occured. Check your network.")
-    
+            exit()
 
-def collateInfo(annotations,ids):
-    annots = {}
-    for annotation in annotations:
-        for header in annotation:
-            if header not in annots:
-                annots[header] = []
-                annots[header].append(annotation[header])
+
+        ## parse results 
+        hs = [
+            "extId",
+            "source",
+            "pmcid",
+        ]
+        sources = [results[h] for h in hs if results.get(h) != None] ## some records do not contain pmcid
+                ## also used .get instead of try except to chatch KeyErrors!! 
+        exacts = [annot["exact"] for annot in results.get("annotations")] ## the exact match to an Organism term
+
+        ## some records do not contain "section" infos
+        sections = []
+        for annot in results["annotations"]:
+            if annot.get("section") != None:
+                sections.append(annot["section"].split(" (")[0])
             else:
-                annots[header].append(annotation[header])
-    headers = list(annots.keys())
-    print("\t".join(headers))
-    # pprint(annots)
-    for i, id_ in enumerate(ids):
-        vals = []
-        for header in headers:
-            if id_ == f'{annots["source"][i]}:{annots["extId"][i]}':
-                if header in ["extId", "pmcid", "source"]:
-                    vals.append(annots[header][i])
-                    # print(vals)
+                sections.append("?No description") # means missing section description
+
+        info = {"id" : id_}
+        info.update(dict(zip(hs, sources)))
+        info_ = {}
+        for s, e in zip(sections, exacts):
+            if s not in info_ :
+                info_[s] = []
+                info_[s].append(e)
+            else:
+                if e.lower() not in map(str.lower,info_[s]):
+                    info_[s].append(e)
+        info.update(info_)
+        infos.append(info)
+
+    # collate results
+
+    ## get headers
+    ## I sampled around 40 ids to scope the headers but
+    ## in case there are other headers and/or using
+    ## more pubids, use the next 4 hashed lines
+    ## and comment out the 'headers' list
+    ## defining headers ahead of time saves another loop
+    ## and also ensures that the headers are sorted
+    ## but may result to some columns being blank in the output
+
+    # _ = []
+    # for info in infos:
+    #     _ += list(info.keys())
+    # headers = list(set(_))
+    
+    headers = ["id",
+        "source",
+        "extId",
+        "pmcid",
+        "Title",
+        "Abstract",
+        "Introduction",
+        "Methods",
+        "Results",
+        "Discussion",
+        "Conclusion",
+        "Article",
+        "Figure",
+        "Table",
+        "Supplementary material",
+        "Author Contributions",
+        "?No description",
+    ] 
+
+    outf_name = "annotations.txt" 
+    with open(outf_name,"w") as outf:
+        print("\t".join(headers), file=outf)
+
+        # collate information
+        for info in infos:
+            vals = []   
+            for header in headers:
+                if info.get(header) != None:
+                    if header in ["extId", "source", "pmcid", "id"]:
+                        vals.append(info[header])
+                    else:
+                        vals.append(", ".join(info[header]))
                 else:
-                    try:
-                        vals.append(", ".join(annots[header][i]))
-                    except IndexError:
-                        vals.append("")
-        print("\t".join(vals))
+                    vals.append("")
+            print("\t".join(vals), file=outf)
+
+    print(f"Retrieved a total of {counter_ids} annotations. Outfile written in '{outf_name}'. Done")
+
+def getIds(path):
+    try:
+        with open(path) as infile:
+            ids = [line.strip() for line in infile.readlines()]
+        print(f"Found {len(ids)} ids")
+        return(ids)
+    except FileNotFoundError:
+        print(f"Error. '{path}' not found.")
+        exit()
 
 
-
-## TO BE DONE
-def getAccessionNumbersInfo(results):
-    annotations = results["annotations"]
-    accession_numbers = []
-    for annotation in annotations:
-        prefix = annotation["prefix"]
-        exact = annotation["exact"]
-        postfix = annotation["postfix"]
-        accession_number = f"{prefix}{exact}{postfix}"
-        if accession_number not in accession_numbers:
-            accession_numbers.append(accession_number)
-    pprint(accession_numbers)
-
+############
+## MAIN ###
+############
 
 if __name__ == "__main__":
+
+    try:
+        query = argv[1]
+    except IndexError:
+        print( """This script accesses the European PMC API using a user-supplied list of publication ids
+    and returns a tab-delimited file containing annotated Organism terms present in each of these publications.  
+    Format of the ids should be <SOURCE>:<ID_NUMBER> e.g. MED:30497052. Multiple ids are separated by
+    new lines. ID_NUMBER should be relevant to the corresponding SOURCE.
+
+    Usage: 
+        python foo.py '<path_to_ids>' 
+            e.g. python foo.py ids.txt
+        Save to file
+            e.g. python foo.py ../samples/ids.txt
+                
+
+    Allowed SOURCEs are:
+        MED: PubMed MEDLINE, 
+        PMC: PubMedCentral not in (PubMed), 
+        PAT: Patents,
+        AGR: Agricola (USDA/NAL), 
+        CBA: Chinese biological abstracts, 
+        HIR: NHS Evidence (UK HIR), 
+        CTX: CiteXplore submission, 
+        ETH: EThOS theses (BL), 
+        CIT: CiteSeer (PSU),
+        PPR: Preprints, 
+        NBK: NLM Books (not in PubMed)
+
+    """)
+        exit()
+
     now = datetime.datetime.now()
     print("Date and time of query:", now)
-    ids = ["MED:26039313","MED:30497052"]
-    type_ = "Organisms"
-    annotations = runRetrieveAnnotation(ids, type_)
-    collateInfo(annotations, ids)
+
+    ids = getIds(argv[1])
+    type_ = "Organisms" ## [MODIFY THIS] Only Organisms annotations are retrieved
+    infos = retrieveAnnotations(ids,type_)
+
+
+
+
